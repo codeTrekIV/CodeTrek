817ed445e4f9f9ba1279dc65e3d1671bffefb224
Louis <louisfilip@gmail.com>
2024-01-16 12:31:16 -0600

    Updating to trigger github actions (#50)

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-    return <h1>Welcome to louisfilip.com</h1>
+    return <h1>Welcome to louisfilip.com!!</h1>
 }
 
 export default Home;
